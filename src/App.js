b1cdfe2e830eb2baf494be4148b2592c6f104b7d
jlow2397 <jlow2397@gmail.com>
2021-01-14 22:33:44 -0800

    initial commit!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import firebase from './firebase';
+
+const db = firebase.firestore();
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    db.collection('users').get()
+      .then((snapshot) => {
+        const fbUsers = snapshot.docs.map(doc => doc.data());
+        this.setState({
+          loading:false,
+          users: fbUsers
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>HSF Encouragement Board</div>
+        {this.state.loading ? <div>Loading</div> :
+          <ul>
+            {this.state.users.map((user, i) => (
+              <li key={i}>
+                Hello {user.name}
+              </li>
+            ))}
+          </ul>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
