1a4b8567808f3eafc7410ec8fa0c335698a5770f
jlow2397 <jlow2397@gmail.com>
2021-01-30 23:52:39 -0800

    Adding react-print to message modal

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,5 +1,6 @@
 import './Messages.css';
-import React, { useState } from 'react';
+import React, { useState, useRef, useMemo } from 'react';
+import { useReactToPrint } from 'react-to-print';
 
 import firebase from 'firebase/app';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
@@ -11,9 +12,16 @@ function Messages(props: any) {
   const letterRef = userRef.collection('messages');
   const [letters] = useCollectionData(letterRef);
   const [envelopeImage, setEnvImg] = useState<Blob>();
+  const [index, setIndex] = useState(0);
 
   const d = new Date();
   const releaseTime = new Date("2021-01-16T12:00:00.01");
+
+  const componentRef = useRef(null);
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
 
   if (d < releaseTime) {
     return (
@@ -53,17 +61,22 @@ function Messages(props: any) {
     }
     else {
       return (
-        <div className="message-modal">
+        <div className="message-container">
           <button className="close x" onClick={props.hideMessages}>x</button>
-          {letters.map((l:any, i:number) => (
-            (l.text.length !== 0) &&
-              <div className="encouragement-letter">
-                {l.text.map((block: any) => (
-                  <p className={block.style}>{block.message}</p>
-                ))}
-                <b>Sent by: {l.displayName}</b>
-              </div>
-          ))}
+          <button className="print-button x" onClick={handlePrint}>Print</button>
+          <div className="message-modal">
+            <div className="message-bg" ref={componentRef}>
+              {letters.map((l:any, i:number) => (
+                (l.text.length !== 0) &&
+                    <div className="encouragement-letter">
+                      {l.text.map((block: any) => (
+                        <p className={block.style}>{block.message}</p>
+                      ))}
+                      <b>Sent by: {l.displayName}</b>
+                    </div>
+              ))}
+            </div>
+          </div>
         </div>
       );
     }
